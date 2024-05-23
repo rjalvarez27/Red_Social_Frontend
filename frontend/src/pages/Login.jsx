286358e068f0df6981c012892e6ad788b8fe3525
Raúl Alvarez <raul509@gmail.com>
2024-05-23 14:50:56 -0400

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,9 @@
 // logica de login
-import React, { useState} from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate, NavLink} from "react-router-dom";
 import axios from "axios";
-import "../styles/inde.css";
+
+
 
 export function Login() {
   const [data, setData] = useState({
@@ -21,7 +22,7 @@ export function Login() {
     }
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/login",
+        "http://localhost:3000/social/login",
         data
       );
       console.log(response.data);
@@ -33,45 +34,55 @@ export function Login() {
     }
   };
 
-  
+
   return (
     <>
+      <div className="containerL">
+        <div className="box1 flex-none w-1/3">
+          <img src="../src/img/login/logo.png" alt="logo" className="w-[150px] m-2" />
+          <h1 className="text-3xl">MOUNTS</h1>
+        </div>
+        <div className="box2">
+          <form className='flex flex-col items-center bg-transparent'>
+            <h3>Correo electronico</h3>
+            <label className='flex items-center border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white'>
+              <input type="Email" placeholder="Ingresa tu Email" className='p-1 rounded-md' />
+            </label>
+            <h3>Contraseña</h3>
+            <label className='flex items-center border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white '>
+              <input type="password" placeholder="Contraseña" className='p-1 rounded-md ' />
+            </label>
+            <div className="flex">
+              <p className="hr">--------------- </p><h1> O </h1><p className="hr"> ---------------</p>
+            </div>
+            <div className="flex">
+              <button className="m-3">
+                <i className="fa-brands fa-x-twitter"></i>
+              </button>
+              <button className="m-3">
+                <i className="fa-brands fa-facebook"></i>
+              </button>
+              <button className="m-3">
+                <i className="fa-brands fa-google"></i>
+              </button>
+            </div>
+            <NavLink to="/recuperar">¿Olviste tu contraseña?</NavLink>
+            <input type="submit" value="Iniciar Sesion" className='bg-black hover:bg-cyan-400 text-white font-bold py-2 px-6 rounded-lg m-8' />
+          </form>
+        </div>
+      </div>
       <div className="container">
-        <h2 className="text-4xl font-semibold m-4 bg-transparent text-black">
-          Inicio
-        </h2>
-        <i className="bx bxs-user-rectangle text-9xl text-cyan-600"></i>
-        <form
-          className="flex flex-col m-3 gap-4 items-center bg-transparent"
-          onSubmit={senData}
-        >
-          <label className="flex items-center border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white">
-            <i className="bx bx-envelope m-2"></i>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Ingresa tu Email"
-              className="p-1 rounded-md"
-              onChange={(e) => setData({ ...data, email: e.target.value })}
-            />
-          </label>
-          <label className="flex items-center border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white ">
-            <i className="bx bx-lock-alt m-2"></i>
-            <input
-              type="password"
-              name="password"
-              placeholder="Contraseña"
-              className="p-1 rounded-md "
-              onChange={(e) => setData({ ...data, password: e.target.value })}
-            />
-          </label>
-          <input
-            type="submit"
-            value="Iniciar Sesión"
-            className="bg-cyan-600 hover:bg-cyan-400 text-white font-bold py-2 px-6 rounded-full m-8"
-          />
-        </form>
+        <div className="box3">
+          <p>¿No tienes cuenta?</p>
+          <NavLink to="/register">Registrate</NavLink>
+        </div>
+      </div>
+      <div className="container">
+        <div className="box4 	position: relative;">
+          <p>Descarga la app</p>
+          <img src="../src/img/login/img2.png" alt="app" className="w-[500px]" />
+          <img src="../src/img/login/img1.png" alt="fondo" className="w-[500px] z-0 " />
+        </div>
       </div>
     </>
   );
