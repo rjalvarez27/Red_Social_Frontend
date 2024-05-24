4a94d5cbdc13655d764837ddf343e08e2a072d7d
Raúl Alvarez <raul509@gmail.com>
2024-05-24 10:42:31 -0400

    update pages
    creacion de las nuevas paginas con su ruta

---
+++ frontend/src/pages/Payments.jsx
@@ -0,0 +1,7 @@
+export function Payments() {
+    return (
+        <div>
+            <h1>Pago</h1>
+        </div>
+    )
+}
---
+++ frontend/src/pages/ratesP.jsx
@@ -0,0 +1,7 @@
+export function Rates () {
+    return(
+        <div>
+            <h1>Rates</h1>
+        </div>
+    )
+}
