f39889069b5cb35a19a75a110eb2b85d3e233894
Raúl Alvarez <raul509@gmail.com>
2024-05-25 15:18:20 -0400

    update css
    responsive pages

--- frontend/src/pages/Payments.jsx
+++ frontend/src/pages/Payments.jsx
@@ -5,12 +5,12 @@ import '../styles/general.css'
 
 export function Payments() {
     return (
-        <div className="general-body">
+        <div>
             <div className="general-content">
                 <div className="general-box1">
-                    <div className="general-part1 ">
-                        <img src="../src/img/principales/logo.png" alt="" className='w-[150px] my-[30px] relative justify-end' />
-                        <ol className='flex-col '>
+                    <img src="../src/img/principales/logo.png" alt="" className='w-[150px] my-[60px]' />
+                    <div className="general-part1">
+                        <ol className='flex flex-col '>
                             <li>Explorar</li>
                             <li >Interacciones</li>
                             <li >Premium</li>
@@ -18,53 +18,46 @@ export function Payments() {
                             <li >Configuracion</li>
                         </ol>
                     </div>
-                    <img src="../src/img/principales/logo.png" alt="" className='w-[150px] my-[60px]' />
+                    <img src="../src/img/principales/logo.png" alt="" className='w-[100px] my-[60px]' />
                 </div>
                 <div className="general-box2">
-                    <div className='flex-col w-[100%]'>
-                        <NavLink to="/" className="flex justify-end"><img src="../src/img/principales/home.png" alt="home" className="w-[60px] m-2" /></NavLink>
-                        <h1 className="text-3xl font-black m-2 text-center">Pago de la Publicidad</h1>
+                    <div className='flex-col'>
+                        <NavLink to="/" className="flex justify-end"><img src="../src/img/principales/home.png" alt="home" className="w-12 m-2" /></NavLink>
+                        <h1 className="text-3xl font-black  text-center m-1">Pago de la Publicidad</h1>
                     </div>
-                    <div className='flex-col w-[100%]'>
-                        <p className="text-xl m-3 text-start">Seleccione su metodo de Pago</p>
-                        <div className='flex-col text-lg text-start m-3'>
+                    <div className='flex-col text-center'>
+                        <p className="m-1 text-justify">Seleccione su metodo de Pago</p>
+                        <div className='flex-col w-full'>
                             <label className='m-2' >
                                 <input type="checkbox" name="basic" id="basic" className=' bg-white  hover:accent-black ' /> Pago movil</label>
                             <label className='m-2'>
                                 <input type="checkbox" name="Premiun" id="Premiun" className=' bg-white  hover:accent-black' /> PayPal</label>
                         </div>
                     </div>
-                    <div className='flex-col w-[100%] text-center '>
-                        <p className="text-lg m-3 text-start" >Si es por paypal por favor Presione el boton para proceder con la forma de pago:</p>
-                        <button className='text-blue-500 bg-yellow-300 w-[200px] m-3 p-2'>PayPal</button>
+                    <div className='flex-col text-center '>
+                        <p className=" m-1 text-justify" >Si es por paypal por favor Presione el boton para proceder con la forma de pago:</p>
+                        <button className='text-blue-500 bg-yellow-300 w-36 m-3 p-2 rounded-md'>PayPal</button>
+                        <p className="text-justify" >Si es por pago Movil verifique que los datos son correctos e ingrese los datos en la siguiente formulario:</p>
                     </div>
-                    <div className='flex-col w-[100%]'>
-                        <p className="text-lg m-3 text-center" >Si es por pago Movil verifique que los datos son correctos e ingrese los datos en la siguiente seccion:</p>
-                    </div>
-                    <div className='flex-col w-[100%]'>
-                        <h3 className="text-lg font-black">Nombre</h3>
-
-                        <label>
-                            <input type="text" className='flex  border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white ' name="name" id="name" />
-                        </label>
-                        <h3 className="text-lg font-black items-start ">Cedula</h3>
-                        <label>
+                    <div className='flex flex-col items-center '>
+                        <form action="" className=' flex flex-col text-center border border-gray-800 rounded-lg  p-4 shadow-lg bg-white'>
+                            <h3 className="text-sm font-black">Nombre</h3>
+                            <input type="text" className='flex border border-gray-800 rounded-lg gap-2 p-1 shadow-lg bg-white ' name="name" id="name" />
+                            <h3 className="text-sm font-black items-start ">Cedula</h3>
                             <input type="text" className='p-1 rounded-md text-center' name="name" id="name" />
-                        </label>
-                        <h3 className="text-lg font-black items-start ">Banco</h3>
-                        <label >
+                            <h3 className="text-sm font-black items-start ">Banco</h3>
                             <input type="text" className='p-1 rounded-md text-center' name="name" id="name" />
-                        </label>
-                        <h3 className="text-lg font-black items-start ">Numero de Referencia</h3>
-                        <label >
+                            <h3 className="text-sm font-black items-start ">Numero de Referencia</h3>
                             <input type="text" className='p-1 rounded-md text-center' name="name" id="name" />
-                        </label>
+                            <p className="text-xs">Verifique antes de Connfirmar</p>
+                            <button className='text-white bg-black  m-1 p-1 rounded-md'>Confirmar</button>
+                        </form>
                     </div>
                     <div className='flex-col w-[100%] text-center '>
-                    <p className="text-lg m-3 text-start">
-                        Si tiene algun problema con el pago por favor ingrese al siguien link
-                    </p>
-                    <button className='text-white bg-green-500 w-[200px] m-3 p-2'>WhatsApp</button>
+                        <p className="text-sm m-3 text-center">
+                            Si tiene algun problema con el pago por favor ingrese al siguien link:
+                        </p>
+                        <button className='text-white bg-green-500 w-36 m-3 p-2'>WhatsApp</button>
                     </div>
                     <div className="sticky top-[100vh] conten3">
                         <h1 className="text-sm text-center m-2 ">© 2024 Copyright: Mounts</h1>
@@ -78,7 +71,7 @@ export function Payments() {
                             <li className='m-2 text-3xl'><i className="fa-solid fa-gear"></i></li>
                         </ul>
                     </div>
-                    <div className='rates-part3'>
+                    <div className='general-part3'>
                         <h1>Mensaje</h1>
                         <p>Aqui va el mensaje</p>
                         <p>Aqui va el mensaje</p>
--- frontend/src/pages/ratesP.jsx
+++ frontend/src/pages/ratesP.jsx
@@ -6,11 +6,11 @@ export function Rates() {
 
 
     return (
-        <div className="general-body">
+        <div>
             <div className="general-content">
-                <div className="general-box1">
+                <div className="general-box1 z-0">
+                    <img src="../src/img/principales/logo.png" alt="" className='w-[150px] my-[60px]' />
                     <div className="general-part1 ">
-                        <img src="../src/img/principales/logo.png" alt="" className='w-[150px] my-[30px] relative justify-end'/>
                         <ol className='flex-col '>
                             <li>Explorar</li>
                             <li >Interacciones</li>
@@ -19,72 +19,72 @@ export function Rates() {
                             <li >Configuracion</li>
                         </ol>
                     </div>
-                    <img src="../src/img/principales/logo.png" alt="" className='w-[150px] my-[60px]' />
+                    <img src="../src/img/principales/logo.png" alt="" className='w-[100px] my-[60px]' />
                 </div>
-                <div className="general-box2">
+                <div className="general-box2 z-40">
                     <div className='flex-col w-[100%]'>
-                        <NavLink to="/" className="flex justify-end"><img src="../src/img/principales/home.png" alt="home" className="w-[60px] m-2" /></NavLink>
+                        <NavLink to="/" className="flex justify-end"><img src="../src/img/principales/home.png" alt="home" className="w-12 m-2" /></NavLink>
                         <h1 className="text-3xl font-black m-2 text-center">Tarifas de Publicidad</h1>
                     </div>
                     <div className='flex'>
                         <table className="w-[100%] text-center m-10 text-lg ">
-                        <thead> 
-                            <tr className=" text-white bg-black ">
-                                <th>Tipos de Publicidad</th>
-                                <th>Basica</th>
-                                <th>Premium</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className="border-b-2" >
-                                <th>1 Publicacion Diaria</th>
-                                <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
-                                <td className='text-green-500'></td>
-                            </tr>
-                            <tr className="border-b-2">
-                                <th>5 Publicaciones Diarias</th>
-                                <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
-                                <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
-                            </tr>
-                            <tr className="border-b-2">
-                                <th>Estadisricas de alcance</th>
-                                <td className='text-green-500'> </td>
-                                <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
-                            </tr>
-                            <tr className="border-b-2">
-                                <th>Apoyo con IA</th>
-                                <td className='text-green-500'></td>
-                                <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
-                            </tr>
-                            <tr className="border-b-2">
-                                <th>Precio</th>
-                                <td className='font-bold'>25$</td>
-                                <td className='font-bold'>27$</td>
-                            </tr>
-                            <tr className="border-b-2">
-                                <th>Seleccione el tipo de Plan</th>
-                                <td ><input type="checkbox" name="basic" id="basic" className=' bg-white  hover:accent-black '/></td>
-                                <td><input type="checkbox" name="Premiun" id="Premiun" className=' bg-white  hover:accent-black' /></td>
-                            </tr>
-                        </tbody>
+                            <thead>
+                                <tr className=" text-white bg-black ">
+                                    <th>Tipos de Publicidad</th>
+                                    <th>Basica</th>
+                                    <th>Premium</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="border-b-2" >
+                                    <th>1 Publicacion Diaria</th>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                    <td className='text-green-500'></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>5 Publicaciones Diarias</th>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Estadisricas de alcance</th>
+                                    <td className='text-green-500'> </td>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Apoyo con IA</th>
+                                    <td className='text-green-500'></td>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Precio</th>
+                                    <td className='font-bold'>5$</td>
+                                    <td className='font-bold'>7$</td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Seleccione el tipo de Plan</th>
+                                    <td ><input type="checkbox" name="basic" id="basic" className=' bg-white  hover:accent-black ' /></td>
+                                    <td><input type="checkbox" name="Premiun" id="Premiun" className=' bg-white  hover:accent-black' /></td>
+                                </tr>
+                            </tbody>
                         </table>
                     </div>
                     <div className='flex text-center mx-5'>
                         <h3>Seleccione el tipo de Plan a contratar Mensual y haga click en el siguiente boton para continuar con el pago</h3>
                     </div>
                     <div className='flex justify-center w-[100%]' >
-                    <NavLink to="/Payments"><button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-8 rounded-lg m-4">Confirmar</button></NavLink>
+                        <NavLink to="/Payments"><button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-8 rounded-lg m-4">Confirmar</button></NavLink>
                     </div>
                     <div className="sticky top-[100vh] conten3">
                         <h1 className="text-sm text-center m-2 ">© 2024 Copyright: Mounts</h1>
                     </div>
                 </div>
-                <div className="general-box3">
-                    <div className= "flex justify-center w-[100%] ">
+                <div className="general-box3 z-0">
+                    <div className="flex justify-center w-[100%] ">
                         <ul className='flex m-5'>
-                            <li className='m-2 text-3xl'><i class="fa-solid fa-bell"></i></li>
-                            <li className='m-2 text-3xl'><i class="fa-solid fa-magnifying-glass"></i></li>
-                            <li className='m-2 text-3xl'><i class="fa-solid fa-gear"></i></li>
+                            <li className='m-2 text-3xl'><i className="fa-solid fa-bell"></i></li>
+                            <li className='m-2 text-3xl'><i className="fa-solid fa-magnifying-glass"></i></li>
+                            <li className='m-2 text-3xl'><i className="fa-solid fa-gear"></i></li>
                         </ul>
                     </div>
                     <div className='general-part3'>
