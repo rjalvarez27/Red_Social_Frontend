98e04cdbecac209eece8ce5f36785f05cbcb6573
Raúl Alvarez <raul509@gmail.com>
2024-05-24 22:32:21 -0400

    update css

--- frontend/src/pages/ratesP.jsx
+++ frontend/src/pages/ratesP.jsx
@@ -73,7 +73,10 @@ export function Rates() {
                         <h3>Seleccione el tipo de Plan a contratar Mensual y haga click en el siguiente boton para continuar con el pago</h3>
                     </div>
                     <div className='flex justify-center w-[100%]' >
-                    <NavLink to="/Payments"><button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-8 rounded-lg m-4">Aceptar</button></NavLink>
+                    <NavLink to="/Payments"><button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-8 rounded-lg m-4">Confirmar</button></NavLink>
+                    </div>
+                    <div className="sticky top-[100vh] conten3">
+                        <h1 className="text-sm text-center m-2 ">© 2024 Copyright: Mounts</h1>
                     </div>
                 </div>
                 <div className="general-box3">
