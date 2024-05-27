98fddfb970e91e8a10188ad80dedc89022c216c5
Raúl Alvarez <raul509@gmail.com>
2024-05-27 19:47:44 -0400

    update

--- frontend/src/pages/RecoverEmail.jsx
+++ frontend/src/pages/RecoverEmail.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, NavLink } from "react-router-dom";
-import { validCorreo} from "../components/Regext.jsx";
+import { validCorreo } from "../components/Regext.jsx";
 import axios from "axios";
 import "../styles/recovery.css";
 import Cookies from 'js-cookie'
@@ -9,7 +9,7 @@ export function RecoverEmail() {
     const navigate = useNavigate()
     const [data, setData] = useState({
         email: "",
-    })  
+    })
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,22 +17,24 @@ export function RecoverEmail() {
             alert('El email se encuentra vacio, por favor rellene la casilla ')
             return
         }
-        if(!validCorreo.test(data.email)){
+        if (!validCorreo.test(data.email)) {
             alert('Por favor verifique el dato ingresado')
         }
         else if (validCorreo.test(data.email)) {
             try {
                 const response = await axios.post(`http://localhost:3000/social/recovery/${data.email}`, data);
-                if(response.data){
-                   console.log(response.data)
-                   alert("Se ha enviado un correo para recuperar su cuenta, Esta pagina lo redirecionara en 3 segundos")
-                   setTimeout(function () {
-                   navigate("/");
-                   }, 3000);
-                return
+                console.log(response.data)
+                if (response) {
+                    alert("Se ha enviado un correo para recuperar su cuenta, Esta pagina lo redirecionara en 3 segundos")
+                    setTimeout(function () {
+                        navigate("/");
+                    }, 3000);
+                    return
+                }else {
+                    alert("Error en servidor")
                 }
             } catch (error) {
-                console.log(error.response.data);
+                alert(error.response.data.message);
             }
         }
     }
@@ -50,7 +52,7 @@ export function RecoverEmail() {
                     <form className='flex-col bg-transparent m-5' onSubmit={handleSubmit}>
                         <h3 className="text-xl font-black m-1">Email*</h3>
                         <label className='flex border border-gray-800 rounded-lg shadow-lg bg-white  m-1'>
-                            <input type="Email" className='p-1 rounded-md text-center w-full  py-2' name="email" id="email" onChange={(e) => setData({ ...data, email: e.target.value })} placeholder=""/>
+                            <input type="Email" className='p-1 rounded-md text-center w-full  py-2' name="email" id="email" onChange={(e) => setData({ ...data, email: e.target.value })} placeholder="" />
                         </label>
                         <p className="text-sm text-justify m-1 font-bold ">Ingrese su correo electronico y presione enviar para recibir un codigo para recuperar su cuenta </p>
                         <div className="flex justify-center m-5 ">
--- frontend/src/pages/RecoverPassword.jsx
+++ frontend/src/pages/RecoverPassword.jsx
@@ -1,35 +1,73 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { useParams , useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { validPassword } from "../components/Regext.jsx";
 import axios from "axios";
 import "../styles/recovery.css";
 
 
 export function RecoverPassword() {
     const navigate = useNavigate();
-    const [code, setCode] = useState(false);
     const params = useParams();
-  
-
-
+    const [passwordC, setPasswordC] = useState("");
+    const [id, setId] = useState("");
+    const [value, setValue] = useState({
+        password: "",
+    })
+    
+    const hanledUser = async (e) => {
+        e.preventDefault();
+        console.log(id)
+        console.log(value.password)
+        console.log(passwordC)
+        console.log(value)
+        
+        if (!value.password || !passwordC) {
+            alert('Campos estan vacios, por favor rellene todos los datos')
+            return
+        }
+        if (value.password != passwordC) {
+            alert('Las contrasenias no coinciden, por favor verifique los datos')
+            return
+        }if (value.password.length < 6) {
+            alert('La contrasenia debe tener minimo 6 caracteres')
+            return
+        } else if(validPassword.test(value.password)) { 
+            console.log("paso esto")
+            try {
+                console.log(`http://localhost:3000/social/user/${id}, value`)
+                const response = await axios.patch(`http://localhost:3000/social/user/${id}`, value);
+                console.log(response)
+                alert("Contrasenia cambiada con exito")
+                setTimeout(function () {
+                    navigate("/");
+                }, 3000);
+            } catch (error) {
+                console.error('error:', error.message);
+            }
+        }
+    }
     useEffect(() => {
         const validToken = async () => {
             try {
                 const response = await axios.get(`http://localhost:3000/social/recovery/${params.code}`);
-                setCode(response.data.message)
-                console.log(code)
-                if(code == true){
-                    console.log("ok")
-                }else if(response.data.message == "jwt malformed"){
-                    navigate('/')
+                if (response) {
+                    setId(response.data.message);
                 }
             } catch (error) {
-                console.log(error.response.data);
-
+                console.log(error.response.data.message);
+                if (error.response.data.message === false) {
+                    alert("El token no es valido o a expirado")
+                    setTimeout(function () {
+                        navigate("/");
+                    }, 2000);
+                }
+                
             }
         };
         validToken();
-    }, []);
+
+    }, [params]);
 
 
     return (
@@ -43,14 +81,14 @@ export function RecoverPassword() {
                 </div>
                 <div className="recovery-box2">
                     <h3 className="text-2xl font-black  text-center">Recuperar Password</h3>
-                    <form className='flex-col bg-transparent m-5'>
+                    <form className='flex-col bg-transparent m-5' onSubmit={hanledUser}>
                         <h3 className="font-black m-1">Introduce tu nueva contraseña</h3>
                         <label className='flex border border-gray-800 rounded-lg shadow-lg bg-white  m-1'>
-                            <input type="Email" className='p-1 rounded-md text-center   py-2' />
+                            <input type="password" name="password" id="password" className='p-1 rounded-md text-center py-2 w-full' onChange={(e) => setValue({ ...value, password: e.target.value })} />
                         </label>
                         <h3 className=" font-black m-1">Introduce tu nueva contraseña</h3>
                         <label className='flex border border-gray-800 rounded-lg shadow-lg bg-white  m-1'>
-                            <input type="Email" className='p-1 rounded-md text-center   py-2' />
+                            <input type="password" name="passwordC" id="passwordC" className='p-1 rounded-md text-center w-full py-2' onChange={(e) => setPasswordC(e.target.value)} />
                         </label>
                         <div className="flex justify-center m-5 ">
                             <input type="submit" value="Enviar" className='bg-black hover:bg-gray-800 text-white font-bold py-2 px-10 rounded-lg' />
