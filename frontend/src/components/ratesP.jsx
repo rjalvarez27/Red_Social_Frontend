cedc519fc761a2d85b21b190f994540e8e042aff
Raúl Alvarez <raul509@gmail.com>
2024-05-28 08:46:58 -0400

    update

---
+++ frontend/src/components/ratesP.jsx
@@ -0,0 +1,135 @@
+//Pagina para las tarifas de Publicidad 
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { NavLink, useNavigate} from 'react-router-dom'
+import '../styles/general.css'
+import Cookies from 'js-cookie'
+
+export function Rates() {
+   const navigate = useNavigate()
+   const[checkB, setCheckB] = useState(false)
+   const[checkP, setCheckP] = useState(false)
+   
+   
+   const handleCheck = () => {
+    if(!checkB && !checkP){
+        alert("Por favor seleccione una tarifa")
+    }
+    if(checkB == true && checkP == true){
+        alert("Estan marcadas las dos casillas, por favor marque solo una casilla")
+   }else if(checkB == true){
+       Cookies.set('basic', 'true')
+       alert ("Tarifa basica activada,  reedirigiendo al pago")
+       setTimeout(() => {
+       navigate('/payments') 
+       }, 2000);
+   } else if(checkP == true){
+    Cookies.set('premium', 'true')
+    alert ("Tarifa Premium activada,  reedirigiendo al pago")
+    setTimeout(() => {
+    navigate('/payments') 
+    }, 2000);
+   }
+}
+    useEffect(() => {
+        const token = Cookies.get('token')
+        if (!token) {
+            navigate('/home')
+        }
+
+    }, [])
+    return (
+        <div>
+            <div className="general-content">
+                <div className="general-box1 z-0">
+                    <img src="../src/img/principales/logo.png" alt="" className='w-[150px] my-[60px]' />
+                    <div className="general-part1 ">
+                        <ol className='flex-col '>
+                            <li>Explorar</li>
+                            <li >Interacciones</li>
+                            <li >Premium</li>
+                            <li >Mensaje</li>
+                            <li >Configuracion</li>
+                        </ol>
+                    </div>
+                    <img src="../src/img/principales/logo.png" alt="" className='w-[100px] my-[60px]' />
+                </div>
+                <div className="general-box2 z-40">
+                    <div className='flex-col w-[100%]'>
+                        <NavLink to="/" className="flex justify-end"><img src="../src/img/principales/home.png" alt="home" className="w-12 m-2" /></NavLink>
+                        <h1 className="text-3xl font-black m-2 text-center">Tarifas de Publicidad</h1>
+                    </div>
+                    <div className='flex'>
+                        <table className="w-[100%] text-center m-10 text-lg ">
+                            <thead>
+                                <tr className=" text-white bg-black ">
+                                    <th>Tipos de Publicidad</th>
+                                    <th>Basica</th>
+                                    <th>Premium</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="border-b-2" >
+                                    <th>1 Publicacion Diaria</th>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                    <td className='text-green-500'></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>5 Publicaciones Diarias</th>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Estadisricas de alcance</th>
+                                    <td className='text-green-500'> </td>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Apoyo con IA</th>
+                                    <td className='text-green-500'></td>
+                                    <td className='text-green-500'><i className="fa-solid fa-square-check"></i></td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Precio</th>
+                                    <td className='font-bold'>5$</td>
+                                    <td className='font-bold'>7$</td>
+                                </tr>
+                                <tr className="border-b-2">
+                                    <th>Seleccione el tipo de Plan</th>
+                                    <td><input type="checkbox" name="Premiun" id="Premiun" className=' bg-white  hover:accent-black hover: text-green-500' onChange={(e) => setCheckB(e.target.checked)} /></td>
+                                    <td><input type="checkbox" name="Premiun" id="Premiun" className=' bg-white  hover:accent-black hover: text-green-500' onChange={(e) => setCheckP(e.target.checked)} /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className='flex text-center mx-5'>
+                        <h3>Seleccione que plan de publicidad desea adquirir por un mes y haga click en confirmar para seguir con el proceso de pago</h3>
+                    </div>
+                    <div className='flex justify-center w-[100%]' >
+                       <button className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-8 rounded-lg m-4" onClick={handleCheck}>Confirmar</button>
+                    </div>
+                    <div className="sticky top-[100vh] conten3">
+                        <h1 className="text-sm text-center m-2 ">© 2024 Copyright: Mounts</h1>
+                    </div>
+                </div>
+                <div className="general-box3 z-0">
+                    <div className="flex justify-center w-[100%] ">
+                        <ul className='flex m-5'>
+                            <li className='m-2 text-3xl'><i className="fa-solid fa-bell"></i></li>
+                            <li className='m-2 text-3xl'><i className="fa-solid fa-magnifying-glass"></i></li>
+                            <li className='m-2 text-3xl'><i className="fa-solid fa-gear"></i></li>
+                        </ul>
+                    </div>
+                    <div className='general-part3'>
+                        <h1>Mensaje</h1>
+                        <p>Aqui va el mensaje</p>
+                        <p>Aqui va el mensaje</p>
+                        <p>Aqui va el mensaje</p>
+                        <p>Aqui va el mensaje</p>
+                        <p>Aqui va el mensaje</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,34 +11,53 @@ import './styles/newpost.css'
 import './styles/explorar.css'
 import './styles/profile.css'
 import './styles/messages.css'
+import "../src/styles/index.css"
 
 import {Home} from './pages/Home.jsx'
 import {Messages} from './pages/Messages.jsx'
 import { Publication } from './pages/Publication.jsx'
 import { Explorar } from './pages/Explorar.jsx'
 import { Profile } from './pages/Profile.jsx'
+import ReactDOM from 'react-dom/client'
+import { PrivateRoute } from './routes/PrivateRoutes'
+import { Login } from '../src/pages/Login.jsx'
+import { Register } from '../src/pages/Register.jsx'
+import { Member } from '../src/pages/Member.jsx'
+import { MembershipPay } from '../src/pages/MembershipPay.jsx'
+import { Payments } from '../src/pages/Payments.jsx'
+import { PerfilAdmin } from '../src/pages/PerfilAdmin.jsx'
+import { PerfilUser } from '../src/pages/PerfilUser.jsx'
+import { Rates } from './pages/ratesP.jsx'
+import { RecoverEmail } from '../src/pages/RecoverEmail.jsx'
+import { RecoverPassword } from '../src/pages/RecoverPassword.jsx'
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-    {/*<Header/>
-    <Nav />
-    <Aside />
-    <FyP />*/}
-
     <BrowserRouter>
         <Routes>
-            
-            <Route path='/' element={<Home/>}/>
-
-            <Route path='/messages' element={<Messages/>}/>
-
-            <Route path='/post' element={<Publication/>}/>
-
-            <Route path='/explorar' element={<Explorar/>}/>
-
-            <Route path='/profile' element={<Profile/>}/>
+        <Route element={<PrivateRoute />}>
+        //Rutas Privadas
+        //aqui van las rutas privadas
+        </Route>
+        //Rutas Publicas
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/member" element={<Member />} />
+        <Route path="/membershipPay" element={<MembershipPay />} />
+        <Route path="/payments" element={<Payments />} />
+        <Route path="/perfiladmin" element={<PerfilAdmin />} />
+        <Route path="/perfiluser" element={<PerfilUser />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path="/recoverEmail" element={<RecoverEmail />} />
+        <Route path="/recoverPassword/:code" element={<RecoverPassword />} />
+        <Route path='/' element={<Home/>}/>
+        <Route path='/messages' element={<Messages/>}/>
+        <Route path='/post' element={<Publication/>}/>
+        <Route path='/explorar' element={<Explorar/>}/>
+        <Route path='/profile' element={<Profile/>}/>
         </Routes>
     </BrowserRouter>
     </React.StrictMode>,
