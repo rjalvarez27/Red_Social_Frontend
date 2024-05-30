4079cabedb6eb06d7045e220d194ea001fa46d11
GabrielMontes4 <josegabrielmontesmarcano@gmail.com>
2024-05-30 15:41:04 -0400

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,7 @@
 import { Header } from '../components/Header.jsx'
 import { Nav } from '../components/Nav.jsx'
 import { Aside } from '../components/Aside.jsx'
-import { FyP } from '../components/FyP.jsx
-import { Newpost } from '../components/Newpost.jsx'
-
-export function Home() {
-  return (
-    <>
-      <Header />
-      <Nav />
-      <Aside />
-      <FyP />
-
-    </>
-
+import { FyP } from '../components/FyP.jsx'
 import Cookies from 'js-cookie'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
