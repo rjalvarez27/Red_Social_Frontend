2bda55a50329e900cbbb5f48b285fbf0e986cd54
Raúl Alvarez <raul509@gmail.com>
2024-06-28 13:03:44 -0400

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,49 +2,43 @@ import Cookies from 'js-cookie'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import '../styles/general.css'
 
 export function Home(){
     const navigate = useNavigate()
-    const token = Cookies.get('token')
-    const [id, setId] = useState()
-    const [user, setUser] = useState()
+    
 
-  useEffect(() => {
-    const hanledToken = async () => {
-        if (!token) {
-            alert('Por favor inicia sesion')
-            setTimeout(function () {
-                navigate("/");
-            }, 2000);
-            return
-        } else {
-            try {
-                const response = await axios.get(`http://localhost:3000/social/recovery/${token}`);
-                setId(response.data.message)
-            } catch (error) {
-                console.error('error:', error.message);
-            }
-        }
-    }
-    const hanledUser = async () => {
-        if (id) {
-            try {
-                const response = await axios.get(`http://localhost:3000/social/user/${id}`);
-                setUser(response.data)
-            } catch (error) {
-                console.error('error:', error.message);
-            }
-        }
-    }
-    hanledToken()
-    hanledUser()
-}, [token, id]);
+
+
+
 
   return(
       <>
-       <div>
-        <p>estoy en home</p>
-       </div>
+        <div>
+            <div className="general-content">
+                <div className="general-box1 z-0">
+                    <img src="" alt="" className='w-[150px] cursor-pointer rounded-full my-10' onClick={() => navigate("/")} />
+                    
+                  
+                </div>
+
+                <div className="general-box2 z-40">
+                    <div className="sticky top-[100vh] conten3">
+                        <h1 className="text-sm text-center m-2 ">© 2024 Copyright: Mounts</h1>
+                    </div>
+                </div>
+                <div className="general-box3 z-0">
+                    <div className="option-space">
+                        <img src="src/images/notification.png" alt="Notificaciones" className="option-space-img" />
+                        <input type="search" name="search" id="search" placeholder="Buscar..." className="option-space-search" />
+                        <img src="src/images/settings.png" alt="Settings" className="option-space-img" onClick={() => setSettings(!settings)} />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
       </>
   )
 }
